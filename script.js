3e2597823a98a5322f99b540edb402a930caeeca
artmazon <49465717+ArtMazon@users.noreply.github.com>
2023-09-26 11:02:00 -0600

    Finished the project with the given conditions, still many things TODO and improve

--- script.js
+++ script.js
@@ -1,56 +1,8 @@
 
 
-
-
-
-function game() {
-    let i = 0;
-    let playerScore = 0;
-    let computerScore = 0;
-    while(i<5){
-
-        let playerChoice = prompt("Make your choice (Rock,Paper,Scissor)").toLowerCase();
-
-        let computerChoice = getComputerChoice();
-
-        let roundResult = playRound(playerChoice,computerChoice);
-
-        playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1,playerChoice.length);
-        computerChoice = computerChoice[0].toUpperCase() + computerChoice.substring(1,computerChoice.length);
-        
-        
-
-        if( roundResult == "invalid" ){
-            console.log("Invalid option, try again")
-            continue;
-        }
-
-        console.log(`Round : ${i+1}`)
-
-        if(roundResult == "Win"){
-            console.log(`You win ! ${playerChoice} beats ${computerChoice}`)
-
-            playerScore++;
-        }
-
-        if(roundResult == "Lose"){
-            console.log(`You Lose ! ${computerChoice} beats ${playerChoice}`)
-            computerScore++;
-        }
-
-        if(roundResult == "Draw"){
-            console.log(`It's a Draw`)
-        }
-
-        i++
-        
-
-
-    }
-
-    return`Final score Player: ${playerScore} Computer: ${computerScore} `
-    
-}
+let playerScore = 0;
+let computerScore = 0
+window.addEventListener("click",playRoundUI);
 
 function playRound(playerChoice,computerChoice) {
 
@@ -141,6 +93,29 @@ function getComputerChoice() {
     
 }
 
+function roundResultText(roundResult,playerChoice,computerChoice) {
+
+    if(roundResult == "Win"){
+
+        playerScore++;
+       return `You win ! ${playerChoice} beats ${computerChoice}`;
+
+    }
+
+    if(roundResult == "Lose"){
+        computerScore++
+       return `You Lose ! ${computerChoice} beats ${playerChoice}`
+
+    }
+
+    if(roundResult == "Draw"){
+        return `It's a Draw`
+    }
+
+
+    
+}
+
 function playRoundUI(e) {
 
     if(!e.target.id){
@@ -149,11 +124,7 @@ function playRoundUI(e) {
 
     const id = e.target.id;
 
-    console.log(e.target.id);
-
     const playerChoice = playerOptionController(id);
-
-    console.log(playerChoice);
 
     const computerChoice = getComputerChoice();
 
@@ -161,19 +132,20 @@ function playRoundUI(e) {
 
     const resultUI = document.querySelector(".result");
 
-    resultUI.textContent = `You ${roundResult} `
+    resultUI.textContent = roundResultText(roundResult,playerChoice,computerChoice);
+
+    const scoreUI =document.querySelector(".score");
+
+    scoreUI.textContent = `Player Score : ${playerScore} , Computer Score : ${computerScore}`;
 
     const playerOption = document.querySelector(`#${e.target.id}`)
-    console.log(e.target.classList[0]);
-    console.log(playerOption);
-   
+  
 
 
     playerOption.classList.add(roundResult.toLocaleLowerCase());
 
     const computerChoiceUI= document.querySelector(`#${computerOtpionController(computerChoice)}`);
 
-    console.log(computerChoiceUI);
     switch (roundResult) {
         case "Win":
             computerChoiceUI.classList.add("lose");
@@ -191,7 +163,23 @@ function playRoundUI(e) {
             break;
     }
 
-    console.log(e.target.classList[1]);
+    if(playerScore == 5 || computerScore == 5 ){
+
+        if(playerScore > computerScore){
+            resultUI.textContent ="Game over, Player Wins";
+
+        }else{
+            resultUI.textContent ="Game over, Computer Wins";
+
+
+        }
+
+
+
+       
+    }
+
+
 }
 
 
@@ -260,4 +248,3 @@ optionBlockList.forEach(optionBlock => optionBlock.addEventListener('transitione
 
 
 
-window.addEventListener("click",playRoundUI);
